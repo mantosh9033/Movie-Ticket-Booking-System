4cd3cc66e88ac37994dfee91bc83df1c549f054e
mantosh9033 <63337450+mantosh9033@users.noreply.github.com>
2022-02-23 01:19:02 +0530

    Add files via upload

---
+++ Movie ticket booking system/js/index.js
@@ -0,0 +1,63 @@
+var timeInter=setInterval(animation,2400);
+var timeInter=setInterval(arrow,2000);
+
+function animation(){
+    for (let index = 1; index <=4; index++) {
+        $(`#txt${index}`).toggle(index*500);
+    }   
+}
+function arrow(){
+    for (let index = 2; index >=1; index--) {
+        $(`#arr${index}`).hide(index*500);
+        $(`#arr${index}`).show(index*500);
+    }  
+}
+$(document).ready(function(){
+    console.log(data);
+    
+    $('#button-5').on('click',function(){
+        $('#login').removeClass("login");
+        $('#poster').hide(1500);
+        $('#firstNavbar').fadeOut(800);
+    });
+    $('#login-form').on('submit',function(event){
+        var name =data.find(function(req,res){
+            if(req.name==$('#inputEmail').val())
+                return true;
+        });
+        var password=data.find(function(req,res){
+            if(req.password==$('#inputPassword').val())
+                return true;
+        });
+        var invalid=`<div><h5 class="text-danger text-uppercase text-muted ">!!! Incorrect Credentials</h5></div>`;
+        if(name&&password){
+            window.location='nowShowing.html';
+        }
+        else{
+            $("#invalid-input").html(invalid);   
+        }
+        event.preventDefault();
+    });
+    $('#go-to-reg').on('click',function(event){
+        $('#login').fadeOut(1000);
+        $('#register').removeClass('register');
+        event.preventDefault();
+    });
+    $('#register-form').on('submit',function(event){
+        var n=$("#name").val();
+        var e=$("#inputEmail2").val();
+        var p=$("#inputPassword2").val();
+        
+        var object={name:n,email:e,password:p};
+        data.push(object);
+        $('#success-reg').html(`<h4 class="pt-4 themecolor"><i>SuccessFull Registeration</i></h4>`)
+
+        event.preventDefault();
+    });
+    $('#go-to-login').on('click',()=>{
+        $('#login').fadeIn(1000);
+        $('#register').addClass('register')
+    });
+    
+});
+
